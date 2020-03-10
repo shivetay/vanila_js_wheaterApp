e10c0be1e3515d4b82fa72fd6627cd6f5d1f627d
Łukasz Dawidowicz <lddev@outlook.com>
2020-03-10 14:43:05 +0100

    add html structure for city

--- js/script.js
+++ js/script.js
@@ -2,10 +2,39 @@
 
 {
   const appInit = function() {
+    const apiKey = 'aba77bdfede6c34d4257aceb705b1452';
     // get dom elements
     const searchButton = document.getElementById('getWheater');
+    const cityInput = document.getElementById('cityInput');
+    const resultDisplay = document.getElementById('wheaterSection');
 
-    console.log('button', searchButton);
+    const fetchCity = (key, cityName) => {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`
+      )
+        .then(res => res.json())
+        .then(data => {
+          renderElements(data);
+        });
+    };
+
+    const renderElements = data => {
+      const createDiv = document.createElement('div');
+      createDiv.innerHTML = `
+      <div>
+        <div>Wheater for ${data.name} </div>
+        <div>
+          
+        </div>
+
+      </div>`;
+      resultDisplay.appendChild(createDiv);
+    };
+
+    searchButton.addEventListener('click', function() {
+      const city = cityInput.value;
+      fetchCity(apiKey, city);
+    });
   };
 
   appInit();
