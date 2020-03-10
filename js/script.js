becf1edfd949da1924f8ab55716e617986467e27
Łukasz Dawidowicz <lddev@outlook.com>
2020-03-10 10:19:58 +0100

    change structure

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+  const appInit = function() {
+    // get dom elements
+    const searchButton = document.getElementById('getWheater');
+
+    console.log('button', searchButton);
+  };
+
+  appInit();
+}
